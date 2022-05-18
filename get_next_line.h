b3bff91870ec77e3da5f7905cfd40d07ae6dc1b1
yudi <yudi@mogari.com.br>
2022-05-19 01:10:08 +0200

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 01:06:46 by eryudi-m          #+#    #+#             */
+/*   Updated: 2022/05/19 01:08:13 by eryudi-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <stdlib.h>
+#include <unistd.h>
+
+
+char *get_next_line(int fd);
+
