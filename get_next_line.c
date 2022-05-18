16648c058aec9e8424d2b58315d9b9ab56ed06cd
yudi <yudi@mogari.com.br>
2022-05-19 01:30:28 +0200

    testing

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,18 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 01:05:03 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/05/19 01:05:26 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/05/19 01:30:09 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	static	int *iptr;
+	int			i; //receives the function read - it depends on BUFFER_SIZE ; fd and the content on that was allocatted within the buffer size
+	char		*ptr;
 
+	int			buffer_size; //this variable needs to be declared when compiling all the program
+	
+}
