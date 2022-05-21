7f211e2d4179fb84ad2d3888691594f64dae6bc4
yudi <yudi@mogari.com.br>
2022-05-21 23:56:44 +0200

    test

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,25 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 01:06:46 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/05/19 01:08:13 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/05/21 22:45:18 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#include <stdlib.h>
-#include <unistd.h>
 
+# include <stdlib.h>
+# include <unistd.h>
 
-char *get_next_line(int fd);
+//main file functions
+char	*get_next_line(int fd);
 
+//utils functions - max of 5 functions per file
+size_t	ft_strlen(const char *string);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+/* it's possible to incorporate the ft_strlcpy in ft_strdup,
+ in case I need to reduce the number of functions */
+size_t	ft_strlcpy(char *dest, const char *src, size_t n);
+
+#endif
