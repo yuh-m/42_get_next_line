b0c738e606268c4ac7b83b29ef42cd590f53aaaf
yudi <yudi@mogari.com.br>
2022-06-06 01:52:11 +0200

    adding utils functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 01:05:03 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/06/06 00:42:14 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/06/06 01:01:10 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 //		1 -> it has to read the file limiting the buffer size \
 //		if there aren't a new line on the oupt
 //			it should read again and concat
-//		if there's a new line it should read
+//		if there's a new line it should read the pointer and concat till the newline and include
 //		2 -> it has to read the file till reaches at a newline \
 //		3 ->  \
 //4 -
@@ -35,8 +35,9 @@ char	*get_buffer(int fd, char *ptr)
 	len = ft_strlen(ptr);
 	ptr = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	i = read(fd, ptr, BUFFER_SIZE);  //i is the number of bytes read
-	if(!ft_strchr(ptr, '\n')
+	if(!ft_strchr(ptr, '\n'))
 		get_buffer(fd, ptr);
+		ft_strlcpy(ptr, get_buffer(fd, ptr), len + 9 );
 	return (ptr);
 }
 
@@ -50,8 +51,15 @@ char	*get_next_line(int fd)
 		return (NULL);
 	if (remainder == NULL)
 		remainder = ft_strdup("");
-	line = ft_strdup(remainder);
+	accumulator = ft_strdup(remainder);
 	line = get_buffer(fd, line);
 
+	free(remainder);
+	remainder = NULL;
+	if (ft_strlen(accumulator == 0)
+	{
+		free(accumulator);
+		return (NULL);
+	}
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 01:06:46 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/05/21 22:45:18 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/06/06 01:05:26 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,11 @@ char	*get_next_line(int fd);
 size_t	ft_strlen(const char *string);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dest, const char *src, size_t n);
+
+//size_t	ft_strlcat(char *dest, const char *src, size_t n)
 /* it's possible to incorporate the ft_strlcpy in ft_strdup,
  in case I need to reduce the number of functions */
-size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 
 #endif
