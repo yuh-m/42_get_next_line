8d86bf7aa65317b0e2537fa9b5db0ba002ca09f8
yudi <yudi@mogari.com.br>
2022-06-22 05:24:04 +0200

    22/06/2022

--- get_next_line.c
+++ get_next_line.c
@@ -6,60 +6,92 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 01:05:03 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/06/06 01:01:10 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/06/22 05:20:50 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
 //remove this before submit
 #include <stdio.h>
-//To do \
-//Done 1 - Main file -> open some file - call gnl to see what happens on the //function \
-//Done 2 - Use GDB so I can focus what's in debbuing whatever I`m going to write \
-//3 - Think on how function should behave \
-//		1 -> it has to read the file limiting the buffer size \
-//		if there aren't a new line on the oupt
-//			it should read again and concat
-//		if there's a new line it should read the pointer and concat till the newline and include
-//		2 -> it has to read the file till reaches at a newline \
-//		3 ->  \
-//4 -
 
-
-char	*get_buffer(int fd, char *ptr)
+char	*get_first_line(const char *line_builder)
 {
-	size_t	len;
+	char	*first_line;
+	int		line_builder_lenght;
 	int		i;
 
-	len = ft_strlen(ptr);
-	ptr = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	i = read(fd, ptr, BUFFER_SIZE);  //i is the number of bytes read
-	if(!ft_strchr(ptr, '\n'))
-		get_buffer(fd, ptr);
-		ft_strlcpy(ptr, get_buffer(fd, ptr), len + 9 );
-	return (ptr);
+	i = 0;
+	line_builder_lenght = ft_strlen(line_builder);
+	first_line = (char *)malloc((line_builder_lenght + 2) * sizeof(char));
+	if (!first_line)
+		return (NULL);
+	ft_strlcpy(first_line, line_builder, line_builder_lenght);
+	return (first_line);
+}
+
+/*
+char	split_line(char **line_builder, char **remainder_previous_buffer, \
+char *newline_char_location)
+{
+	char *next_line;
+
+	next_line = ft_strdup("");
+	//next_line = *remainder_previous_buffer;
+	//free(next_line); the order of when the memory is being free it doesn`t matter on this case, also next_line jsut need to be an empy memory
+	//*remainder_previous_buffer = ft_strdup(newline_char_location + 1); added to gnl
+	//free(next_line);
+	next_line = get_first_line(*line_builder)
+	free(*line_builder);
+	return(next_line);
+
+} */
+
+void	*manager_buffer(char **line_builder, int fd)
+{
+	char	*buffer;
+	int		bytes_read;
+
+	bytes_read = BUFFER_SIZE;
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while ((bytes_read == BUFFER_SIZE) && \
+	(ft_strchr(*line_builder, '\n') == NULL))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+			break;
+		buffer[bytes_read] = '\0';
+		*line_builder = ft_strjoin(*line_builder, buffer);
+		//join_buffer_into_line(line_builder, buffer);
+	}
+	free(buffer);
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*remainder;
-	char		*accumulator;
-	char		*line;
+	static char	*remainder_previous_buffer;
+	char		*line_builder;
+	char		*newline_char_location;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (remainder == NULL)
-		remainder = ft_strdup("");
-	accumulator = ft_strdup(remainder);
-	line = get_buffer(fd, line);
-
-	free(remainder);
-	remainder = NULL;
-	if (ft_strlen(accumulator == 0)
+	if (remainder_previous_buffer == NULL)
+		remainder_previous_buffer = ft_strdup("");
+	line_builder = ft_strdup(remainder_previous_buffer);
+	manager_buffer(&line_builder, fd);
+	newline_char_location = ft_strchr(line_builder, '\n');
+	if (newline_char_location != NULL)
 	{
-		free(accumulator);
+		remainder_previous_buffer = ft_strdup(newline_char_location + 1);
+		return get_first_line(line_builder);
+	}
+	free(remainder_previous_buffer);
+	remainder_previous_buffer = NULL;
+	if (ft_strlen(line_builder) == 0)
+	{
+		free(line_builder);
 		return (NULL);
 	}
-	return (line);
+	return (line_builder);
 }
+ //functin to free memory from variables
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 01:06:46 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/06/06 01:05:26 by eryudi-m         ###   ########.fr       */
+/*   Updated: 2022/06/07 00:01:46 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,12 @@
 //main file functions
 char	*get_next_line(int fd);
 
+
 //utils functions - max of 5 functions per file
 size_t	ft_strlen(const char *string);
 char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dest, const char *src, size_t n);
-
-//size_t	ft_strlcat(char *dest, const char *src, size_t n)
-/* it's possible to incorporate the ft_strlcpy in ft_strdup,
- in case I need to reduce the number of functions */
 
 #endif
