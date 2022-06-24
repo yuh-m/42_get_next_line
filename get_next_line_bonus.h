4f0fb04b26124d1633b9d6b5fd89087233493a1b
yudi <yudi@mogari.com.br>
2022-06-24 22:46:27 +0200

    get_next_line - with bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eryudi-m <eryudi-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/19 01:06:46 by eryudi-m          #+#    #+#             */
-/*   Updated: 2022/06/24 16:03:51 by eryudi-m         ###   ########.fr       */
+/*   Created: 2022/05/19 01:08:55 by eryudi-m          #+#    #+#             */
+/*   Updated: 2022/06/24 21:36:45 by eryudi-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# define MAX_FD 1024
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -24,7 +25,7 @@ char	*get_first_line(char **line_builder, char **remainder_previous_buffer, \
 void	*manager_buffer(char **line_builder, int fd);
 void	join_buffer_into_line(char **line_builder, char *buffer);
 
-//utils functions - max of 5 functions per file
+//utils functions
 size_t	ft_strlen(const char *string);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
